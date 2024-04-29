2e380bcbfe023b51a4fbebed9c8bc6a8b24590cc
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-29 23:41:39 +0300

    initial commit

--- models/tool.js
+++ models/tool.js
@@ -11,7 +11,7 @@ const toolSchema = new mongoose.Schema({
 toolSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    delete returnedObject.id
+    delete returnedObject._id
     delete returnedObject.__v
   }
 })
