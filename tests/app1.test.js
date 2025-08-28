0b15c2f528bb661dae23da09d9bea27fb20f80a6
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2025-08-28 23:53:15 +0300

    Update app1.test.js

--- tests/app1.test.js
+++ tests/app1.test.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const Tool = require('../models/tool')
 const supertest = require('supertest')
+const Tool = require('../models/tool')
 const app = require('../app')
 const helper = require('./tests-helper')
 
@@ -9,14 +9,13 @@ const api = supertest(app)
 beforeEach(async () => {
   await Tool.deleteMany({})
 
-  for (let tool of helper.toolsList) {
-    const toolObject = new Tool(tool)
-    await toolObject.save()
+  for (const tool of helper.toolsList) {
+    await new Tool(tool).save()
   }
 })
 
-describe('when there are initially some tools', () => {
-  test('tools are returned as json', async () => {
+describe('when some tools already exist in the database', () => {
+  test('tools are returned as JSON', async () => {
     await api.get('/api/tools')
       .expect(200)
       .expect('Content-Type', /application\/json/)
@@ -26,72 +25,81 @@ describe('when there are initially some tools', () => {
     const response = await api.get('/api/tools')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+
     expect(response.body).toHaveLength(helper.toolsList.length)
   })
 
-  test('a specific tool is among the returned tools', async () => {
+  test('a specific tool is included in the results', async () => {
     const response = await api.get('/api/tools')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-    const names = response.body.map(t => t.name)
+
+    const names = response.body.map(tool => tool.name)
     expect(names).toContain(helper.toolsList[0].name)
   })
 })
 
-describe('fetching a single too', () => {
-  test('succeeds when given a valid id', async () => {
+describe('fetching a single tool', () => {
+  test('succeeds with a valid id', async () => {
     const toolsAtStart = await helper.toolsInDb()
-    const response = await api.get(`/api/tools/${toolsAtStart[0].id}`)
+    await api.get(`/api/tools/${toolsAtStart[0].id}`)
       .expect(200)
       .expect('Content-Type', /application\/json/)
   })
 
-  test('fails with statuscode 400 when given an invalid id', async () => {
-    const invalidId = 'nfeidjw'
-    const response = await api.get(`/api/tools/${invalidId}`)
+  test('fails with 400 when id is invalid', async () => {
+    await api.get('/api/tools/invalid-id')
       .expect(400)
   })
 
-  test('fails with statuscode 400 when given a nonexistent id', async () => {
+  test('fails with 404 when tool does not exist', async () => {
     const nonexistentId = await helper.nonexistentId()
-    const response = await api.get(`/api/tools/${nonexistentId}`)
+    await api.get(`/api/tools/${nonexistentId}`)
       .expect(404)
   })
 })
 
-describe('addition of a new tool', () => {
-  test('succeeds when given valid data', async () => {
+describe('adding a new tool', () => {
+  test('succeeds with valid data', async () => {
     const toolsAtStart = await helper.toolsInDb()
-    const response = await api.post(`/api/tools/`)
+
+    await api.post('/api/tools/')
       .send(helper.validData)
       .expect(201)
       .expect('Content-Type', /application\/json/)
+
     const toolsAtEnd = await helper.toolsInDb()
-    const names = toolsAtEnd.map(t => t.name)
+    const names = toolsAtEnd.map(tool => tool.name)
+
     expect(names).toContain(helper.validData.name)
     expect(toolsAtEnd).toHaveLength(toolsAtStart.length + 1)
   })
 
-  test('fails with statuscode 400 when given invalid data', async () => {
+  test('fails with 400 when data is invalid', async () => {
     const toolsAtStart = await helper.toolsInDb()
-    const response = await api.post(`/api/tools/`)
+
+    await api.post('/api/tools/')
       .send(helper.invalidData)
       .expect(400)
+
     const toolsAtEnd = await helper.toolsInDb()
-    const names = toolsAtEnd.map(t => t.name)
+    const names = toolsAtEnd.map(tool => tool.name)
+
     expect(names).not.toContain(helper.validData.name)
     expect(toolsAtEnd).toHaveLength(toolsAtStart.length)
   })
 })
 
-describe('deleting of a tool', () => {
-  test('succeeds with statuscode 204',async() => {
+describe('deleting a tool', () => {
+  test('succeeds with status code 204', async () => {
     const toolsAtStart = await helper.toolsInDb()
-    const response = await api.delete(`/api/tools/${toolsAtStart[0].id}`)
-      .send(helper.invalidData)
+
+    await api.delete(`/api/tools/${toolsAtStart[0].id}`)
       .expect(204)
+
     const toolsAtEnd = await helper.toolsInDb()
-    const names = toolsAtEnd.map(t => t.name)
+    const names = toolsAtEnd.map(tool => tool.name)
+
     expect(names).not.toContain(toolsAtStart[0].name)
     expect(toolsAtEnd).toHaveLength(toolsAtStart.length - 1)
   })
