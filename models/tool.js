c583f252525fc7ec0e2d9b04756da7e4a203bf01
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2025-08-30 23:09:28 +0300

    Update tool.js

--- models/tool.js
+++ models/tool.js
@@ -2,17 +2,16 @@ const mongoose = require('mongoose')
 
 const toolSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: String, required: true },
   size: { type: String, required: true },
   price: { type: Number, required: true },
   isAvailable: { type: Boolean, default: false },
 })
 
 toolSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString()
+    delete ret._id
+    delete ret.__v
   }
 })
 
